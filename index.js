2626e734d391c1abdeb734b419ba7d43e91a627a
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2024-10-23 20:26:51 +0200

    blue rects move to javascript

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
-let blueRect1 = $("#southWesternBlueSquare1 rect");
-blueRect1.on("mouseover", () => {
-  blueRect1.toggleClass("originalFill");
-  blueRect1.toggleClass("mouseOverRect");
+//North-eastern blue square group
+for (let i = 10; i > 0; i--) {
+  const blueSquareSvg_ne = `<svg
+        id="northEasternBlueSquare${i}"
+        width="40"
+        height="40"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          class="originalFill"
+          x="10"
+          y="10"
+          height="20"
+          width="20"
+          transform="rotate(45 20 20 )"
+        ></rect>
+      </svg>`;
+  $("#title").prepend(blueSquareSvg_ne);
+}
+
+//South-western blue square group
+for (let i = 6; i > 0; i--) {
+  const blueSquareSvg_sw = `<svg
+        id="southWesternBlueSquare${i}"
+        width="40"
+        height="40"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          class="originalFill"
+          x="10"
+          y="10"
+          height="20"
+          width="20"
+          transform="rotate(45 20 20 )"
+        ></rect>
+      </svg>`;
+  $("#title").prepend(blueSquareSvg_sw);
+}
+
+//mouse-over blue rects animation
+const blueRects = $("#title rect");
+blueRects.on("mouseover", () => {
+  blueRects.toggleClass("originalFill");
+  blueRects.toggleClass("mouseOverRect");
   setTimeout(() => {
-    blueRect1.toggleClass("originalFill");
-    blueRect1.toggleClass("mouseOverRect");
+    blueRects.toggleClass("originalFill");
+    blueRects.toggleClass("mouseOverRect");
   }, 100);
 });
