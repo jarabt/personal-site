7df95ff25e2cd023e09c9bcce64ee85de000c59d
Jaroslav Tesar <jaroslavtesar@Jaroslav--MacBook-Pro.local>
2024-10-23 18:41:26 +0200

    adds jQuery

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-var blueRect1 = document.querySelector("#southWesternBlueSquare1 rect");
-blueRect1.addEventListener("mouseover", function () {
-  this.classList.toggle("originalFill");
-  this.classList.toggle("mouseOverRect");
-  var myRect = this;
-  setTimeout(function () {
-    myRect.classList.toggle("originalFill");
-    myRect.classList.toggle("mouseOverRect");
+let blueRect1 = $("#southWesternBlueSquare1 rect");
+blueRect1.on("mouseover", () => {
+  blueRect1.toggleClass("originalFill");
+  blueRect1.toggleClass("mouseOverRect");
+  setTimeout(() => {
+    blueRect1.toggleClass("originalFill");
+    blueRect1.toggleClass("mouseOverRect");
   }, 100);
 });
